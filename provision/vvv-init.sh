b376d72e19d2475b574878918b1b3a56a17efc71
emmills3 <emmills3@users.noreply.github.com>
2019-09-30 09:23:44 -0400

    Update vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -6,9 +6,49 @@ WP_REPO=`get_config_value 'wp_repo' ''`
 PARENT_THEME_REPO=`get_config_value 'parent_theme_repo' ''`
 CHILD_THEME_REPO=`get_config_value 'child_theme_repo' ''`
 
-echo ${WP_REPO}
-echo ${PARENT_THEME_REPO}
-echo ${CHILD_THEME_REPO}
+echo "${WP_REPO}"
+echo "${PARENT_THEME_REPO}"
+echo "${CHILD_THEME_REPO}"
+
+if [ "${WP_REPO}" != "" ]; then
+  echo "Yes"
+fi
+
+if [ "${WP_REPO}" = "" ]; then
+  echo "blank"
+fi
+
+if [ ! "${WP_REPO}" ]; then
+  echo "last"
+fi
+
+if [ "${WP_REPO}" ]; then
+  echo "final"
+fi
+
+if [ -z "${WP_REPO}" ]; then
+  echo "z1"
+fi
+
+if [ ${WP_REPO} != "" ]; then
+  echo "Yes22"
+fi
+
+if [ ${WP_REPO} = "" ]; then
+  echo "blan22k"
+fi
+
+if [ ! ${WP_REPO} ]; then
+  echo "la222st"
+fi
+
+if [ ${WP_REPO} ]; then
+  echo "fi22nal"
+fi
+
+if [ -z ${WP_REPO} ]; then
+  echo "z122"
+fi
 
 # Fetch the first host as the primary domain. If none is available, generate a default using the site name
 DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
